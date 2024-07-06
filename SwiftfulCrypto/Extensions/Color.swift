f412da9e41ba549c8a8df7a505292c1b2bfd153b
Eduard Marrugo <marruguera@gmail.com>
2024-07-06 10:47:03 +0300

    [Clean] Add images and custom color sheme

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Admin on 06.07.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
