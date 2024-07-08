fad976fcbdc298523bee0624779b2e635d84a89f
Eduard Marrugo <marruguera@gmail.com>
2024-07-08 14:33:18 +0300

    [Feature] made CoinImageView

---
+++ SwiftfulCrypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageView.swift
+//  SwiftfulCrypto
+//
+//  Created by Admin on 08.07.2024.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject private var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+             if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+    }
+}
