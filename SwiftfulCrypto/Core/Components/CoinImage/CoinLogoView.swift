6395301a1ef46e3411b13d9d4aa5fa027ec02ea7
Eduard Marrugo <marruguera@gmail.com>
2024-07-10 13:58:49 +0300

    [Feature] made CoinLogoView

---
+++ SwiftfulCrypto/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinLogoView.swift
+//  SwiftfulCrypto
+//
+//  Created by Admin on 10.07.2024.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinLogoView()
+    }
+}
