ae58ca037aa0976b82799d99a872578e6c6909f6
Eduard Marrugo <marruguera@gmail.com>
2024-07-10 12:33:44 +0300

    [Feature] add real data to HomeStatView

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,7 +22,9 @@ class HomeViewModel: ObservableObject {
     
     @Published var searchBarText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
+    
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -33,12 +35,19 @@ class HomeViewModel: ObservableObject {
         
         // updates allCoins
         $searchBarText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .map (mapGlobalMarketData)
+            .sink { [weak self] returnedData in
+                self?.stat = returnedData
             }
             .store(in: &cancellables)
     }
@@ -56,4 +65,26 @@ class HomeViewModel: ObservableObject {
         return filteredText
         
     }
+    
+    private func mapGlobalMarketData(data: MarketDataModel?) -> [StatisticModel] {
+        
+        var stat: [StatisticModel] = []
+
+        guard let data = data else { return stat }
+
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, persentage: data.marketCapChangePercentage24HUsd)
+        let totalVolume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", persentage: 0)
+
+        stat.append(contentsOf: [
+            marketCap,
+            totalVolume,
+            btcDominance,
+            portfolio
+        ])
+
+        return stat
+    }
+    
 }
