7d2b590dab76b15ea3347cec31f10e163a8ca97c
Eduard Marrugo <marruguera@gmail.com>
2024-07-15 13:10:11 +0300

    [Feature] setup overview and additional view parts

--- SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -21,19 +21,77 @@ struct DetailView: View {
     
     @StateObject private var vm: DetailViewModel
     
+    private var columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
     }
     
     var body: some View {
-        VStack {
-            Text("tmp")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("Hiu")
+                    .frame(height: 150)
+                    .background(Color.indigo)
+                overview
+                Divider()
+                additionalDetails
+            }
+            .padding()
+        }
+        .navigationTitle(vm.coin.name)
+    }
+}
+
+extension DetailView {
+    private var overview: some View {
+        Group {
+            Text("Overview")
+                .font(.title)
+                .bold()
+                .foregroundColor(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Divider()
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: nil,
+                      pinnedViews: []) {
+                ForEach(vm.overviewStat) { stat in
+                    StatisticBarView(stat: stat)
+                }
+            }
+        }
+    }
+    
+    private var additionalDetails: some View {
+        Group {
+            Text("Additional Details")
+                .font(.title)
+                .bold()
+                .foregroundColor(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Divider()
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: nil,
+                      pinnedViews: []) {
+                ForEach(vm.additionalStat) { stat in
+                    StatisticBarView(stat: stat)
+                }
+            }
         }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
