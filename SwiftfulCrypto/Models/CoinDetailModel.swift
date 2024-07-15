a4a4cb1f697cb8ae7f2df5952187398b56d0b3a2
Eduard Marrugo <marruguera@gmail.com>
2024-07-15 13:09:18 +0300

    [Bug] make model struct

--- SwiftfulCrypto/Models/CoinDetailModel.swift
+++ SwiftfulCrypto/Models/CoinDetailModel.swift
@@ -97,39 +97,35 @@
 
 import Foundation
 
-class CoinDetailModel: Codable {
+
+struct CoinDetailModel: Identifiable, Codable {
+    let id, symbol, name: String?
+    let blockTimeInMinutes: Int?
+    let hashingAlgorithm: String?
+    let categories: [String]?
+    let description: Description?
+    let links: Links?
     
-    struct Welcome {
-        let id, symbol, name: String?
-        let blockTimeInMinutes: Int?
-        let hashingAlgorithm: String?
-        let categories: [String]?
-        let description: Description?
-        let links: Links?
-        
-        
-        enum CodingKeys: String, CodingKey {
-            case id, symbol, name
-            case blockTimeInMinutes = "block_time_in_minutes"
-            case hashingAlgorithm = "hashing_algorithm"
-            case categories
-            case description, links
-        }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name
+        case blockTimeInMinutes = "block_time_in_minutes"
+        case hashingAlgorithm = "hashing_algorithm"
+        case categories
+        case description, links
     }
+}
+
+struct Description: Codable {
+    let en: String?
+}
+
+struct Links: Codable {
+    let homepage: [String]?
+    let subredditURL: String?
     
-    struct Description {
-        let en: String?
+    enum CodingKeys: String, CodingKey {
+        case homepage
+        case subredditURL = "subreddit_url"
     }
-    
-    struct Links {
-        let homepage: [String]?
-        let subredditURL: String?
-        
-        enum CodingKeys: String, CodingKey {
-            case homepage
-            case subredditURL = "subreddit_url"
-        }
-    }
-    
-    
 }
