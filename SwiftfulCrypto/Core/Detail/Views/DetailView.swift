b956f4b8b4be9a841f2a4c25b9c069efff5cd017
Eduard Marrugo <marruguera@gmail.com>
2024-07-15 11:22:59 +0300

    [Feature] connect DetailViewModel to DetailView

--- SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -19,14 +19,15 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
     
-    let coin: CoinModel?
+    @StateObject private var vm: DetailViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+    }
     
     var body: some View {
-        if let coin = coin {
-            VStack {
-                Text(coin.name)
-                Text(coin.symbol)
-            }
+        VStack {
+            Text("tmp")
         }
     }
 }
